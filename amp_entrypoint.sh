af06dd327351faaeeb993e8c7d9455781f977dd3
Peter Schulze <petschul@cisco.com>
2019-12-03 16:20:57 -0700

    install the requirements normally to use fab bin

--- amp_entrypoint.sh
+++ amp_entrypoint.sh
@@ -3,4 +3,5 @@ sudo mkdir -p /opt/splunk/lib/python2.7/site-packages
 sudo mkdir -p /opt/splunk/lib/python3.7/site-packages
 sudo pip install -r requirements.txt --target=/opt/splunk/lib/python2.7/site-packages
 sudo pip install -r requirements.txt --target=/opt/splunk/lib/python3.7/site-packages
+sudo pip install -r requirements.txt
 source /sbin/entrypoint.sh $@
