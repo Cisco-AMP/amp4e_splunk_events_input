2c8ca8ee2cd76e1b27c786d6bcda74c2a6ef711b
Ankit Bhagat <ankitbhagat@nyu.edu>
2021-02-25 15:37:27 -0800

    Making test paths right

--- test/amp4e_events_input/test_amp_storage_wrapper.py
+++ test/amp4e_events_input/test_amp_storage_wrapper.py
@@ -2,6 +2,7 @@ import unittest
 import json
 import sys
 from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
+sys.path.insert(0, make_splunkhome_path(["etc", "apps", "amp4e_events_input"]))
 sys.path.insert(0, make_splunkhome_path(["lib","python3","site-packages"]))
 from splunklib.client import Service, KVStoreCollection
 
