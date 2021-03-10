15c8215415d36da4fac9c7333e62239f7b81c12d
Samson Nguyen <samsnguy@cisco.com>
2021-03-09 17:24:17 -0700

    Change mock to be env dependant

--- test/support/mock_definitions.py
+++ test/support/mock_definitions.py
@@ -1,3 +1,4 @@
+import os
 # Generates validation/input definitions as if they were created by splunk for tests
 class MockDefinitions(object):
     def __init__(self, session_key=None):
@@ -5,7 +6,6 @@ class MockDefinitions(object):
 
     @property
     def metadata(self):
-        host = os.getenv('SPLUNK_API_HOST', 'localhost')
-        port = os.getenv('SPLUNK_API_PORT', 8089),
-        return {'server_uri': 'https://{host}:{port}/', 'session_key': self.session_key,
+        host = os.getenv('SPLUNK_API_HOST', '127.0.0.1')
+        return {'server_uri': 'https://{host}:8089/'.format(host=host), 'session_key': self.session_key,
                 'name': 'amp4e_events_test_input'}
