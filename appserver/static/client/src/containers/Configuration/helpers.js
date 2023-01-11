883817ab05e4acb5b06f73fdc49843ecc074f967
Danylo Mezentsev <dmezents@cisco.com>
2023-01-11 20:19:54 +0200

    dmeze: Fix Config form

--- appserver/static/client/src/containers/Configuration/helpers.js
+++ appserver/static/client/src/containers/Configuration/helpers.js
@@ -1,12 +1,12 @@
 import { locale } from "@splunk/splunk-utils/config"
 
 export const handleValidate = ({ apiHost, apiId, apiKey }) =>
-  apiHost.length && apiId.length && apiKey.length
+  apiHost?.length && apiId?.length && apiKey?.length
     ? {}
     : {
-        apiHost: !apiHost.length && "Is required",
-        apiId: !apiId.length && "Is required",
-        apiKey: !apiKey.length && "Is required"
+        apiHost: !apiHost?.length && "Is required",
+        apiId: !apiId?.length && "Is required",
+        apiKey: !apiKey?.length && "Is required"
       }
 
 export const createInitialValues = ({ apiHost, apiId, apiKey }) => ({
