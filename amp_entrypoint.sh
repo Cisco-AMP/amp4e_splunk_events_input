5cf25aabc089574ab514a17768c252747a79f4c9
mstoro <mstoro@softserveinc.com>
2023-01-18 22:33:14 +0200

    Python packages install fix

--- amp_entrypoint.sh
+++ amp_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo python3 -m pip install -r requirements.txt --target=/usr/lib/python3.7/site-packages
-python3 -m pip install -r bin/requirements-splunk.txt --target=/opt/splunk/lib/python3.7/site-packages
+sudo python -m pip install -r requirements.txt --target=/usr/lib/python3.7/site-packages
+python -m pip install -r bin/requirements-splunk.txt --target=/opt/splunk/lib/python3.7/site-packages
 source /sbin/entrypoint.sh $@
